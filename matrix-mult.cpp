abc95a626349e12cbbadee8303f4c36ec1b06bb3
Dimitar Milev <milev.1996@gmail.com>
2018-05-07 14:16:48 +0300

    Classic matrix multiplication working properly in multithreaded enviroment

--- matrix-mult.cpp
+++ matrix-mult.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <boost/program_options.hpp>
+
+using std::cout;
+using std::endl;
+using std::thread;
+
+namespace po = boost::program_options;
 
 void classic_matrix_mult(double** A, double** B, double** C, int nFrom, int n, int mFrom, int m, int kFrom, int k) 
 {
@@ -35,17 +42,41 @@ void print_matrix(double** A, int n, int m)
     {
         for (int j = 0; j < m; j++)
         {
-            std::cout << A[i][j] << " ";
+            cout << A[i][j] << " ";
         }
-        std::cout << std::endl;
+        cout << endl;
     }
 }
-// try with non sharing matrices
+
+double** copy_matrix(double** M, int n, int m)
+{  
+    double** R = new double*[n];
+    for (int i = 0; i < n; i++)
+    {
+        R[i] = new double[m];
+        for (int j = 0; j < m; j++)
+        {
+            R[i][j] = M[i][j];
+        }
+    }
+    return R;
+}
 // add boost parameters parsing
-int main() 
+int main(int argc, char **argv) 
 {   
+    
+
+
+
+
+
+
+
+
+
+
     auto start = std::chrono::high_resolution_clock::now();
-    int t = 4;
+    int t = 2;
     int n = 2048, m = 2048, k = 2048;
     double **A = new double*[n]; 
     double **B = new double*[m]; 
@@ -62,20 +93,16 @@ int main()
     }
     generate_random_matrix(A, n, m);
     generate_random_matrix(B, m, k);
-    auto gen = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> genElapsed = gen - start;
-    std::cout << "time: " << genElapsed.count() << std::endl;
-
+    
     //print_matrix(A, n, m);
     //print_matrix(B, m, k);
     //classic_matrix_mult(A, B, C, 0, 2, 0, m, 0, k);
-    std::thread threads[t];
+    thread threads[t];
     for (int i = 0; i < t; i++)
     {   
         int nFrom = (i * n) / t;
         int nTo = ((i + 1) * n) / t;
-        std::cout << nFrom << " " << nTo << std::endl;
-        threads[i] = std::thread(&classic_matrix_mult, A, B, C, nFrom , nTo, 0, m, 0, k);
+        threads[i] = thread(&classic_matrix_mult, A, B, C, nFrom , nTo, 0, m, 0, k);
     }
 
     for (int i = 0; i < t; i++)
@@ -85,5 +112,5 @@ int main()
     //print_matrix(C, n, k);
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed = end - start;
-    std::cout << "time: " << elapsed.count() << std::endl;
+    cout << "time: " << elapsed.count() << endl;
 }
