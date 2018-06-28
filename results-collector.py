b1ed3d0c1d0bc36e7107abb7886cbca076ad55c9
Dimitar Milev <milev.1996@gmail.com>
2018-06-28 19:45:26 +0300

    improved results-collector

--- results-collector.py
+++ results-collector.py
@@ -3,9 +3,21 @@ import re
 
 execution_times = []
 
-for i in range(1, 33):
-    output = subprocess.check_output(["./matrix-mult.o -m 1024 -n 1024 -k 1024 -q -t " + str(i)], shell=True)
-    m = re.search("\d+", output.split('\n')[1])
-    execution_times.append(m.group(0))
+def mean(numbers):
+    return float(sum(numbers)) / len(numbers)
+
+def calculate_average(t, number_of_executions=5):
+    results = []
+
+    for i in range(5):
+        output = subprocess.check_output(["./matrix-mult.o -m 512 -n 512 -k 512 -q -t " + str(t)], shell=True)
+        m = re.search("\d+", output.split('\n')[1])
+        results.append(m.group(0))
+        print(output)
+
+    return mean([int(r) for r in results])
+
+for t in range(1, 3):
+    execution_times.append(calculate_average(t))
     
 print(execution_times)
