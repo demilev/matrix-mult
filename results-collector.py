13fbd2db8f3a6209cdf54e84cbe729e08767aac3
demilev <u81352@t5600.yaht.net>
2018-06-28 19:58:15 +0300

    output results to file logic added

--- results-collector.py
+++ results-collector.py
@@ -13,11 +13,10 @@ def calculate_average(t, number_of_executions=5):
         output = subprocess.check_output(["./matrix-mult.o -m 512 -n 512 -k 512 -q -t " + str(t)], shell=True)
         m = re.search("\d+", output.split('\n')[1])
         results.append(m.group(0))
-        print(output)
 
     return mean([int(r) for r in results])
 
-for t in range(1, 3):
-    execution_times.append(calculate_average(t))
-    
-print(execution_times)
+with open("results.txt", "w+") as results_file:
+    results_file.write("Num of threads,Elapsed time\n")
+    for t in range(1, 17):
+        results_file.write(str(t) + "," + str(calculate_average(t)) + "\n")
